7ea751f1355ffd027a7e71ba98e969103933f5ef
Marek Baczynski <marek.bacz@gmail.com>
2016-11-27 11:54:09 +0200

    version update

--- Assets/cup.cs
+++ Assets/cup.cs
@@ -16,6 +16,11 @@ public class cup : MonoBehaviour {
 	private bool isBeingLookedAt;
 	private float timestamp;
 
+	public float delayTime = 0f;
+	private bool triggered = false;
+
+	public AudioSource ping;
+
 	// Use this for initialization
 	void Start () {
 		gaze = gameObject.GetComponent<GazeAware> ();
@@ -27,16 +32,24 @@ public class cup : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
+		if (triggered && Time.time > delayTime) {
+			DemoController.AdvanceDemo ();
+			gameObject.transform.Rotate (0.5f, 0.5f, 0.3f);
+			isBeingLookedAt = false;
+			triggered = false;
+		}
+
 		if (DemoController.step == activeStep) {
 			if (isBeingLookedAt) {
 				targetMesh.material = lookingAt;
 				if (gaze.HasGazeFocus == false) {
 					isBeingLookedAt = false;
 				}
-				if (Time.time - timestamp > 0.7f) {
-					DemoController.AdvanceDemo ();
-					gameObject.transform.Rotate (0.5f, 0.5f, 0.3f);
-					isBeingLookedAt = false;
+				if (Time.time - timestamp > 0.7f && triggered == false) {
+					triggered = true;
+					ping.Play ();
+					delayTime += Time.time;
 				} 
 
 			} else {
---
+++ Assets/hidethis.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hidethis : MonoBehaviour {
+
+
+	public KeyCode trigger;
+	private MeshRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (trigger)) {
+			rend.enabled = !rend.enabled;
+		}
+	}
+}
